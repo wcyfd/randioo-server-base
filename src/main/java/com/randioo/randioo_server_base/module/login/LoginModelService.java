e60aad3731c79553927daea65ce8f3d4a39a6e8e
aimfd <1101697681@qq.com>
2016-12-11 15:00:32 +0800

    update

--- src/main/java/com/randioo/randioo_server_base/module/login/LoginModelService.java
+++ src/main/java/com/randioo/randioo_server_base/module/login/LoginModelService.java
@@ -2,7 +2,6 @@ package com.randioo.randioo_server_base.module.login;
 
 import org.apache.mina.core.session.IoSession;
 
-import com.google.protobuf.GeneratedMessage;
 import com.randioo.randioo_server_base.module.BaseServiceInterface;
 
 public interface LoginModelService extends BaseServiceInterface{
