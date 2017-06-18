0ba352bab9c4508fb4488b35f5cb00f2d3a30b16
wcyfd <1101697681@qq.com>
2017-06-18 11:12:09 +0800

    该版本是该项目不使用分支管理的最后一次更新

--- src/main/java/com/randioo/randioo_server_base/utils/SessionUtils.java
+++ src/main/java/com/randioo/randioo_server_base/utils/SessionUtils.java
@@ -1,10 +1,14 @@
 package com.randioo.randioo_server_base.utils;
 
 import org.apache.mina.core.session.IoSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.randioo.randioo_server_base.cache.SessionCache;
 
 public class SessionUtils {
+	private static Logger logger = LoggerFactory.getLogger(SessionUtils.class.getSimpleName());
+
 	public static void sc(IoSession session, Object message) {
 		if (session != null) {
 			session.write(message);
@@ -12,8 +16,9 @@ public class SessionUtils {
 	}
 
 	public static void sc(int roleId, Object message) {
+		logger.trace("roleId=" + roleId + " " + message);
 		IoSession session = SessionCache.getSessionById(roleId);
 		sc(session, message);
 	}
-	
+
 }
